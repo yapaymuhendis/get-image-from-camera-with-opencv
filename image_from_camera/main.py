43c739be483b7387b1759470f97a1b79a575d436
cagan <109431950+yapaymuhendis@users.noreply.github.com>
2022-10-01 16:41:17 +0300

    Update main.py

--- image_from_camera/main.py
+++ image_from_camera/main.py
@@ -1,30 +1,30 @@
 import cv2
 
-cap = cv2.VideoCapture("C:\\Users\\Administrator\\Desktop\\OpenCV dersleri\\videos\\video.mp4")
-# webcam'den görüntü almak için 0 yazıyoruz.
-# mesela bir video çalıştırmak isteseydik 0 yerine o videonun yolunu yazardık.
-# örnek: cap = cv2.VideoCapture("C:\\Users\\Administrator\\Desktop\\OpenCV dersleri\\videos\\video.mp4")
+cap = cv2.VideoCapture("videos/video.mp4")
+# To get video from camera: cv2.VideoCapture(0)
+# For external camera: cv2.VideoCapture(1)
+
 
 while True:
     ret, frame = cap.read()
 
     frame = cv2.flip(frame, 0)
-    # çıkan çıktı ters gözükür normalde. onun için cv2.flip kullanıyoruz.
+    # We use cv2.flip because the output looks upside down.
+    # If you use your own camera instead of video, you don't need to write cv2.flip.
+    
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # burda ise frame'i yani cap'te okunan görüntü frame olduğu için frame'yi işledik mesela gri yaptık.
+    # here, we processed the frame, that is, the image read in the cap, as the frame, for example, we made it gray.
 
     cv2.imshow("webcam", gray_frame)
-    cv2.imshow("webcam", hsv_frame)
-
-
-
+   
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+    # When I press the q key, I say stop the program from running.
 
 cap.release()
-# video ile işim bitti, serbest bırak demek istiyorum burda.
+# I'm done with the video, I mean release it here.
+
 cv2.destroyAllWindows()
 
 
